5c6567b55ed366cade898760fbf1108ddb9e7331
unknown <mounikareddygonae15@gmail.com>
2020-12-06 22:56:48 +0530

    fetch api & display

--- src/components/App.js
+++ src/components/App.js
@@ -3,13 +3,43 @@ import Navbar from './Navbar';
 import MovieList from './MovieList';
 
 class App extends React.Component {
+  constructor(props) {
+    super(props); 
+        this.state = {
+            movies: [],
+            isLoaded: false,
+            // imageUrl: 'https://image.tmdb.org/t/p/w500'
+        }
+    }
   
-render() {
+
+  async componentDidMount () {
+    const res = await fetch('https://api.themoviedb.org/3/movie/popular?api_key=c20cd7ee3a2836b554ed904651005ff9&language=en-US&page=1')
+    const mov = await res.json();
+    const results = mov.results;
+    console.log(results);
+   this.setState({
+       movies:results,
+       isLoaded:true
+   })
+  }
+ render() {
+     const { movies, isLoaded} = this.state;
+     if(!isLoaded) {
+        return <div>Loading....</div>
+     }
   return (
     <div className="App">
     <Navbar />
     <div className= "main">
-    <MovieList/>
+    {movies.map((movie, index) => {
+          return <MovieList 
+          movie = {movie} 
+        //   imageUrl = {imageUrl}
+          key={`movies-${index}`
+        }
+          />
+        })}
     </div>
     </div>
   );
--- src/components/MovieList.js
+++ src/components/MovieList.js
@@ -4,13 +4,18 @@ import React from 'react';
 class MovieList extends React.Component {
 
   render() {
-  
+    const { movie} = this.props;
+    
   return (
-  <div className="movie-card">
+    <div className="movie-list">
+    <div className="movie-card">
+    <img className="movie-image" src={`https://image.tmdb.org/t/p/w500/${movie.poster_path}`} alt="movie-poster"/>
+    <h2 className="rating">{movie.vote_average} / 10</h2>
+    <button className="button">View Details</button>
+    </div>
+    <h4 className ="movie-title">{movie.title}</h4>
+    <p>{movie.release_date}</p>
    
-    <img className="movie-image"/>
-    <h4 className ="movie-title"></h4>
-    <p></p>
    </div>
   );
 }
